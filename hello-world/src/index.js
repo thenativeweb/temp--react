ba6cf0827d12a0891994d466072f3567351e892d
Golo Roden <golo.roden@thenativeweb.io>
2022-04-26 10:25:44 +0200

    Fix semicolon.

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const $root = document.getElementById('root')
+const $root = document.getElementById('root');
 const root = ReactDOM.createRoot($root);
 
 const App = function () {
